f54b89eedd1bf88c88a62c76b74187f1f29c008c
Anthonybell19 <70494464+Anthonybell19@users.noreply.github.com>
2021-10-12 16:03:07 -0400

    trying to fix bst project 1

--- project1.py
+++ project1.py
@@ -50,164 +50,36 @@ class TreeNode:
         if self.hasRightChild():
             self.rightChild.parent = self
 
-
-class BinarySearchTree:
-
-    def __init__(self, stockList: list):
-        self.root = None
-        self.size = 0
-
-    def length(self):
-        return self.size
-
-    def __len__(self):
-        return self.size
-
-    def put(self, key, val):
-        if self.root:
-            self._put(key, val, self.root)
-        else:
-            self.root = TreeNode(key, val)
-        self.size = self.size + 1
-
-    def _put(self, key, val, currentNode):
-        if key < currentNode.key:
-            if currentNode.hasLeftChild():
-                self._put(key, val, currentNode.leftChild)
-            else:
-                currentNode.leftChild = TreeNode(key, val, parent=currentNode)
-        else:
-            if currentNode.hasRightChild():
-                self._put(key, val, currentNode.rightChild)
-            else:
-                currentNode.rightChild = TreeNode(key, val, parent=currentNode)
-
-    def __setitem__(self, k, v):
-        self.put(k, v)
-
-    def get(self, key):
-        if self.root:
-            res = self._get(key, self.root)
-            if res:
-                return res.payload
-            else:
-                return None
-        else:
-            return None
-
-    def _get(self, key, currentNode):
-        if not currentNode:
-            return None
-        elif currentNode.key == key:
-            return currentNode
-        elif key < currentNode.key:
-            return self._get(key, currentNode.leftChild)
-        else:
-            return self._get(key, currentNode.rightChild)
-
-    def __getitem__(self, key):
-        return self.get(key)
-
-    def __contains__(self, key):
-        if self._get(key, self.root):
-            return True
-        else:
+    def insert(self, key, data):
+        if self.value == data:
             return False
-
-    def delete(self, key):
-        if self.size > 1:
-            nodeToRemove = self._get(key, self.root)
-            if nodeToRemove:
-                self.remove(nodeToRemove)
-                self.size = self.size - 1
-            else:
-                raise KeyError('Error, key not in tree')
-        elif self.size == 1 and self.root.key == key:
-            self.root = None
-            self.size = self.size - 1
-        else:
-            raise KeyError('Error, key not in tree')
-
-    def __delitem__(self, key):
-        self.delete(key)
-
-    def spliceOut(self):
-        if self.isLeaf():
-            if self.isLeftChild():
-                self.parent.leftChild = None
-            else:
-                self.parent.rightChild = None
-        elif self.hasAnyChildren():
+        elif key < self.key:
             if self.hasLeftChild():
-                if self.isLeftChild():
-                    self.parent.leftChild = self.leftChild
-                else:
-                    self.parent.rightChild = self.leftChild
-                self.leftChild.parent = self.parent
+                return self.leftChild.insert(key,data)
             else:
-                if self.isLeftChild():
-                    self.parent.leftChild = self.rightChild
-                else:
-                    self.parent.rightChild = self.rightChild
-                self.rightChild.parent = self.parent
-
-    def findSuccessor(self):
-        succ = None
-        if self.hasRightChild():
-            succ = self.rightChild.findMin()
+                self.leftChild = TreeNode(key, data)
+                return True
         else:
-            if self.parent:
-                if self.isLeftChild():
-                    succ = self.parent
-                else:
-                    self.parent.rightChild = None
-                    succ = self.parent.findSuccessor()
-                    self.parent.rightChild = self
-        return succ
-
-    def findMin(self):
-        current = self
-        while current.hasLeftChild():
-            current = current.leftChild
-        return current
-
-    def remove(self, currentNode):
-        if currentNode.isLeaf():  # leaf
-            if currentNode == currentNode.parent.leftChild:
-                currentNode.parent.leftChild = None
+            if self.hasRightChild():
+                return self.rightChild.insert(key,data)
             else:
-                currentNode.parent.rightChild = None
-        elif currentNode.hasBothChildren():  # interior
-            succ = currentNode.findSuccessor()
-            succ.spliceOut()
-            currentNode.key = succ.key
-            currentNode.payload = succ.payload
+                self.rightChild = TreeNode(key, data)
+                return True
 
-        else:  # this node has one child
-            if currentNode.hasLeftChild():
-                if currentNode.isLeftChild():
-                    currentNode.leftChild.parent = currentNode.parent
-                    currentNode.parent.leftChild = currentNode.leftChild
-                elif currentNode.isRightChild():
-                    currentNode.leftChild.parent = currentNode.parent
-                    currentNode.parent.rightChild = currentNode.leftChild
-                else:
-                    currentNode.replaceNodeData(currentNode.leftChild.key,
-                                                currentNode.leftChild.payload,
-                                                currentNode.leftChild.leftChild,
-                                                currentNode.leftChild.rightChild)
+    def find(self, key):
+        if self.key == key:
+            return True
+        elif key < self.key:
+            if self.hasLeftChild():
+                return self.leftChild.find(key)
             else:
-                if currentNode.isLeftChild():
-                    currentNode.rightChild.parent = currentNode.parent
-                    currentNode.parent.leftChild = currentNode.rightChild
-                elif currentNode.isRightChild():
-                    currentNode.rightChild.parent = currentNode.parent
-                    currentNode.parent.rightChild = currentNode.rightChild
-                else:
-                    currentNode.replaceNodeData(currentNode.rightChild.key,
-                                                currentNode.rightChild.payload,
-                                                currentNode.rightChild.leftChild,
-                                                currentNode.rightChild.rightChild)
+                return False
+        else:
+            if self.hasRightChild():
+                return self.rightChild.find(key)
+            else:
+                return False
+
 
 
 """
@@ -254,7 +126,6 @@ class StockLibrary:
         self.size = len(self.stockList)
         self.isSorted = False
         self.bst = None
-
     """
     The loadData method takes the file name of the input dataset,
     and stores the data of stocks into the library. 
@@ -343,17 +214,13 @@ class StockLibrary:
     """
 
     def buildBST(self):
-        self._put(0, len(self.stockList))
+        for i in self.stockList:
+            if self.bst:
+                return self.bst.insert(i.symbol, i.val)
+            else:
+                self.bst = TreeNode(i.symbol, i.val)
+                return True
 
-    def _put(self, start, end):
-        if start < end:
-            mid = (start + end) // 2
-            node = TreeNode(mid, self.stockList[mid])
-            node.leftChild = self._put(start, mid - 1)
-            node.rightChild = self._put(mid + 1, end)
-            if start == 0 and end == len(self.stockList):
-                self.bst = node
-            return node
 
     """
     Search a stock based on the symbol attribute. 
@@ -361,22 +228,11 @@ class StockLibrary:
     or a “Stock not found” message when there is no match. 
     """
 
-    # work
-    def _find(self, key, currentNode):
-        if currentNode is not None:
-            if key == currentNode.payload.symbol:
-                return currentNode.payload
-            if key < currentNode.payload.symbol:
-                if currentNode.hasLeftChild:
-                    return self._find(key, currentNode.leftChild)
-            elif currentNode.hasRightChild:
-                return self._find(key, currentNode.rightChild)
-        else:
-            return 'stock not found'
+
 
     def searchBST(self, query, current='dnode'):
         if self.bst:
-            return self._find(query, self.bst)
+            return self.bst.find(query)
         else:
             return 'stock not found'
 
