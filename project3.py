f5529a0301972ae703f3448392bd810f3e488956
Anthonybell19 <anthonybell@umass.edu>
2021-12-14 16:17:36 -0500

    started project 3

--- project3.py
+++ project3.py
@@ -4,7 +4,8 @@ import matplotlib.pyplot as plt
 class HousePrices:
     def __init__(self):
         self.data = []
-        self.weights = []
+        self.weights = np.ones(26)
+        self.gradientArray = []
         self.priceList = []
         self.predPriceList = []
         self.totalRecords = 0
@@ -12,7 +13,7 @@ class HousePrices:
         self.maxPrice = 0
         self.mean = 0
         self.standardDeviation = 0
-        self.loss = 0
+        self.lossValue = 0
 
     def loadData(self, filename: str):
         file = open(filename, 'r')
@@ -62,28 +63,58 @@ class HousePrices:
             TotalBsmtSf.append(float(tempList[8]))
             FullBath.append(float(tempList[14]))
 
-        plt.scatter(GrLivArea,BedroomAbvGr,TotalBsmtSf, FullBath, )
-        # plt.hist(self.priceList, bins='auto', range = (self.minPrice, self.maxPrice))
-        plt.gca().set(title='Price Histogram', ylabel='Price');
-        plt.show()
+        # plt.scatter(GrLivArea,BedroomAbvGr,TotalBsmtSf, FullBath, )
+        plt.hist(self.priceList, bins='auto', range = (self.minPrice, self.maxPrice))
+        fig, ax = plt.subplots(2, 3, sharex='col', sharey='row')
+        ax[0,0].scatter(GrLivArea, BedroomAbvGr)
+        ax[0,0].set_xlabel('GrLivArea')
+        ax[0, 0].set_ylabel('BedroomAbvGr')
+        ax[0, 1].scatter(GrLivArea, TotalBsmtSf)
+        ax[0, 2].scatter(GrLivArea, FullBath)
+        ax[1, 0].scatter(BedroomAbvGr, TotalBsmtSf)
+        ax[1, 1].scatter(BedroomAbvGr, FullBath)
+        ax[1, 2].scatter(TotalBsmtSf, FullBath)
 
-    def pred(self, weights):
+        plt.show()
+        plt.gca().set(title='Price Histogram', ylabel='Price');
+        # plt.show()
+
+    def pred(self):
         predPriceList = []
         for i in self.data:
             price = 0
             features = i.split(',')
-            for j in features:
-                featureWeight = features[j] * weights[j]
+            for j in range(1,len(features)-1):
+                featureWeight = float(features[j]) * self.weights[j]
                 price += featureWeight
             predPriceList.append(price)
         self.predPriceList = predPriceList
+
     def loss(self):
         loss = 0
         for i in range(0, len(self.priceList)):
             loss += (self.priceList[i] - self.predPriceList[i])**2
-        self.loss = loss / self.totalRecords
+        self.lossValue = loss / self.totalRecords
+    def gradient(self):
+        gradient = np.zeros(26)
+        gradient[0] = 1
+        for i in range(1, len(gradient)):
+            gradient[i] = self.weights[i] * self.lossValue
+        gradient = gradient / (2 * self.totalRecords)
+        self.gradient = gradient
+        print(gradient)
+    def update(self):
+        for i in range(1, len(self.weights)):
+            self.weights[i] = self.weights[i] - 0.2* self.gradient[i]
 
 if __name__ == '__main__':
     house = HousePrices()
     house.loadData('train.csv')
     house.plotData()
+    house.pred()
+    print(house.predPriceList)
+    house.loss()
+    print(house.lossValue)
+    house.gradient()
+    house.update()
+    print(house.weights)
